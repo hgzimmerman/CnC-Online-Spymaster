9145039a585642464eb64262596e4db4f49ff6b7
ziggypop <zimhen7@gmail.com>
2015-12-27 20:59:08 -0600

    Made the liscence dialog only have an "OK" button. Before, it had a cancel one as well.

--- app/src/main/java/com/mooo/ziggypop/candconline/SettingsActivity.java
+++ app/src/main/java/com/mooo/ziggypop/candconline/SettingsActivity.java
@@ -1,12 +1,16 @@
 package com.mooo.ziggypop.candconline;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
@@ -33,11 +37,33 @@ public class SettingsActivity extends PreferenceActivity {
     }
 
 
-    public static class SettingsFragment extends PreferenceFragment {
+    public static class SettingsFragment extends PreferenceFragment{
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
 
+
+            Preference dialogPref = getPreferenceScreen().findPreference("dialog_preference");
+            dialogPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    builder.setTitle(R.string.licence);
+                    builder.setMessage(R.string.BSD);
+                    builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User clicked OK button
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    return true;
+                }
+            });
+
+
         }
+
+
     }
 }
